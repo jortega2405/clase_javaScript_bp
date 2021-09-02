3d1f54df655e0a37245dd8b1210438708929b8b5
Jose Ortega <jose.ort98@gmail.com>
2021-09-02 13:02:31 -0500

    Hola mundo, variables y constantes

---
+++ script.js
@@ -0,0 +1,55 @@
+//Hello world
+//alert ('Hello world');
+
+//comentarios de una linea
+
+/* 
+comentario 
+multilinea
+*/
+
+//Variables
+
+let mensaje;
+mensaje = "Hello people";
+// alert(mensaje);
+
+mensaje = "Hi";
+// alert(mensaje);
+
+mensaje = "Jose";
+// alert(mensaje);
+
+let a = 3, b = 4, c = 5;
+console.log(a + b);
+
+let name = "Jose";
+let lastname = "Ortega";
+
+console.log("Hola",name,lastname);
+
+//Nombrar Variables
+
+//Camel Case
+let nameOne = "Jose";
+let nameTwo = "Ortega";
+
+//underscore
+let name_one = "Jose";
+let name_two = "Ortega";
+
+//Nombres no comunes
+let $ = 1;
+let _ = 2;
+console.log($,_);
+
+//No puedo nombrar variables
+//let 1a = 2;
+//let primer-nombre = 45;
+
+//Constantes
+const PI = 3.142624;
+const COLOR_NEGRO = "FFFFF";
+
+PI = 4;
+console.log(PI);
